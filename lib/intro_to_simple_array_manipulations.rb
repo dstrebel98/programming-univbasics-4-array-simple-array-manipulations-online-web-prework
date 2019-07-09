1ecc9c115c6cf4803f14ed0e160a9a72dddc0969
Erika N. Fairley <efairley98@gmail.com>
2019-07-09 05:22:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,12 @@ artists.push
 artists.unshift
 artists.pop
 artists.pop
-artists.pop
+artists.pop args 
+artists.shift 
+artists. shift args 
+artists.concat
+artists.insert 
+artist.uniq 
+artists.flatten 
+artists.delete 
+artists.interger
